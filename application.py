5e1b042ebd1f892ff333fc9694630e5fa978e618
julieHzl <49508232+julieHzl@users.noreply.github.com>
2019-05-28 22:20:30 +0800

    Update application.py

--- application.py
+++ application.py
@@ -9,8 +9,7 @@ app = Flask(__name__)
 @app.route('/latest/online-win/')
 def winonline(version):
     target_url = "https://vscjavaci.blob.core.windows.net/vscodejavainstaller/latest/VSCodeJavaInstaller-online-win-%s.exe"%version
-    return target_url
-    #return redirect(target_url, code=302)
+    return redirect(target_url, code=302)
 
 @app.route('/latest/win-x64-jdk8/')
 def jdk8winx64(version):
@@ -41,3 +40,4 @@ def jdk11winx86(version):
 def jdk12winx86(version):
     target_url = "https://vscjavaci.blob.core.windows.net/vscodejavainstaller/latest/VSCodeJava12Installer-win-x86-%s.exe"%version
     return redirect(target_url, code=302)
+
