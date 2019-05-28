a7e87da3f370729456497ebe1fcf7e442bdef9c6
julieHzl <49508232+julieHzl@users.noreply.github.com>
2019-05-28 22:28:56 +0800

    Delete version parameter

--- application.py
+++ application.py
@@ -12,32 +12,32 @@ def winonline():
     return redirect(target_url, code=302)
 
 @app.route('/latest/win-x64-jdk8/')
-def jdk8winx64(version):
+def jdk8winx64():
     target_url = "https://vscjavaci.blob.core.windows.net/vscodejavainstaller/latest/VSCodeJava8Installer-win-x64-%s.exe"%version
     return redirect(target_url, code=302)
 
 @app.route('/latest/win-x64-jdk11/')
-def jdk11winx64(version):
+def jdk11winx64():
     target_url = "https://vscjavaci.blob.core.windows.net/vscodejavainstaller/latest/VSCodeJava11Installer-win-x64-%s.exe"%version
     return redirect(target_url, code=302)
 
 @app.route('/latest/win-x64-jdk12/')
-def jdk12winx64(version):
+def jdk12winx64():
     target_url = "https://vscjavaci.blob.core.windows.net/vscodejavainstaller/latest/VSCodeJava12Installer-win-x64-%s.exe"%version
     return redirect(target_url, code=302)
 
 @app.route('/latest/win-x86-jdk8/')
-def jdk8winx86(version):
+def jdk8winx86():
     target_url = "https://vscjavaci.blob.core.windows.net/vscodejavainstaller/latest/VSCodeJava8Installer-win-x86-%s.exe"%version
     return redirect(target_url, code=302)
 
 @app.route('/latest/win-x86-jdk11/')
-def jdk11winx86(version):
+def jdk11winx86():
     target_url = "https://vscjavaci.blob.core.windows.net/vscodejavainstaller/latest/VSCodeJava11Installer-win-x86-%s.exe"%version
     return redirect(target_url, code=302)
 
 @app.route('/latest/win-x86-jdk12/')
-def jdk12winx86(version):
+def jdk12winx86():
     target_url = "https://vscjavaci.blob.core.windows.net/vscodejavainstaller/latest/VSCodeJava12Installer-win-x86-%s.exe"%version
     return redirect(target_url, code=302)
 
