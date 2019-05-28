b73d993975765f976355037ad20e056337bec18d
Zunli Hu <zuh@microsoft.com>
2019-05-28 15:41:00 +0800

    Add subdirectory

--- application.py
+++ application.py
@@ -1,6 +1,17 @@
-from flask import Flask
+import os
+from flask import Flask,redirect
+
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
 def hello():
-    return "Hello World!"
+    return 'Hello World'
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
