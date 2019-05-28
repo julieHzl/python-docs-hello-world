d0e3a59a0fe80c0eaa153c7a4f59f74596c9abf1
Zunli Hu <zuh@microsoft.com>
2019-05-28 19:20:10 +0800

    read version from app settings

--- application.py
+++ application.py
@@ -1,41 +1,42 @@
 import os
 from flask import Flask,redirect
 import sys
+import urllib.request
 
-version = "0.1.0"
+version = os.environ['version']
 app = Flask(__name__)
 
 @app.route('/latest/online-win/')
-def wiononline():
+def winonline(version):
     target_url = "https://vscjavaci.blob.core.windows.net/vscodejavainstaller/latest/VSCodeJavaInstaller-online-win-%s.exe"%version
     return redirect(target_url, code=302)
 
 @app.route('/latest/win-x64-jdk8/')
-def jdk8winx64():
+def jdk8winx64(version):
     target_url = "https://vscjavaci.blob.core.windows.net/vscodejavainstaller/latest/VSCodeJava8Installer-win-x64-%s.exe"%version
     return redirect(target_url, code=302)
 
 @app.route('/latest/win-x64-jdk11/')
-def jdk11winx64():
+def jdk11winx64(version):
     target_url = "https://vscjavaci.blob.core.windows.net/vscodejavainstaller/latest/VSCodeJava11Installer-win-x64-%s.exe"%version
     return redirect(target_url, code=302)
 
 @app.route('/latest/win-x64-jdk12/')
-def jdk12winx64():
+def jdk12winx64(version):
     target_url = "https://vscjavaci.blob.core.windows.net/vscodejavainstaller/latest/VSCodeJava12Installer-win-x64-%s.exe"%version
     return redirect(target_url, code=302)
 
 @app.route('/latest/win-x86-jdk8/')
-def jdk8winx86():
+def jdk8winx86(version):
     target_url = "https://vscjavaci.blob.core.windows.net/vscodejavainstaller/latest/VSCodeJava8Installer-win-x86-%s.exe"%version
     return redirect(target_url, code=302)
 
 @app.route('/latest/win-x86-jdk11/')
-def jdk11winx86():
+def jdk11winx86(version):
     target_url = "https://vscjavaci.blob.core.windows.net/vscodejavainstaller/latest/VSCodeJava11Installer-win-x86-%s.exe"%version
     return redirect(target_url, code=302)
 
 @app.route('/latest/win-x86-jdk12/')
-def jdk12winx86():
+def jdk12winx86(version):
     target_url = "https://vscjavaci.blob.core.windows.net/vscodejavainstaller/latest/VSCodeJava12Installer-win-x86-%s.exe"%version
     return redirect(target_url, code=302)
