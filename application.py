8397137771c6361e27af99742d0e9851e2ba0627
Zunli Hu <zuh@microsoft.com>
2019-05-28 19:23:56 +0800

    test version

--- application.py
+++ application.py
@@ -9,7 +9,8 @@ app = Flask(__name__)
 @app.route('/latest/online-win/')
 def winonline(version):
     target_url = "https://vscjavaci.blob.core.windows.net/vscodejavainstaller/latest/VSCodeJavaInstaller-online-win-%s.exe"%version
-    return redirect(target_url, code=302)
+    return target__url
+    #return redirect(target_url, code=302)
 
 @app.route('/latest/win-x64-jdk8/')
 def jdk8winx64(version):
